2bab2e1d61612f986145f4ccc7f7d107dc3090e2
MateoBaer99 <91203006+MateoBaer99@users.noreply.github.com>
2022-08-24 16:36:49 +0200

    Update calculGrp3.java

--- moduleOne/src/calculGrp3.java
+++ moduleOne/src/calculGrp3.java
@@ -1,2 +1,14 @@
 public class calculGrp3 {
+     public static double addition(double n1, double n2) {
+        double resultat = n1 + n2;
+        return resultat;
+    }
+    public static double pourcentage(double partie, double total) {
+        double resultatpourcentage = (partie / 100) * total;
+        return resultatpourcentage;
+    }
+    public static double cosinus(double chiffre) {
+        double rest = Math.cos(chiffre);
+        return rest;
+    }
 }
