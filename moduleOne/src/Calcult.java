04cefa4bd593b7631c1edc95196974fc2a391511
Rakhx <audren.bouadjio@gmail.com>
2022-08-24 14:03:22 +0200

    FOrme

---
+++ moduleOne/src/Calcult.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Calcult {
+    private JButton button1;
+    private JButton button2;
+    private JTextField textChiffreOne;
+    private JTextField textChiffreDeux;
+    private JLabel Resultat;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JTextField textField1;
+}
