44f4fd6f754d09eaa9818ad8c2fbd0499a0b0d32
jerom <jeromeleguerroue@gmail.com>
2022-08-24 16:20:55 +0200

    modif gro2

--- moduleOne/src/CalculGrp2.java
+++ moduleOne/src/CalculGrp2.java
@@ -1,2 +1,16 @@
 public class CalculGrp2 {
+    public static double racineCarree(){
+        double a = 9;
+        System.out.println(Math.sqrt(a));
+        return a;
+
+    }
+    public static double sinus(){
+        double c = 0;
+        // converting values to radians
+        double b = Math.toRadians(c);
+        System.out.println(Math.sin(b));
+        return b;
+    }
+
 }
